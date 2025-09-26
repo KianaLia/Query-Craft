44a0f1aa9556a49c28a4d3b661c1e5c7bad1824c
Kiana Liaghat <k.liaghat@jabama.com>
2025-09-26 12:17:01 +0330

    added tempview to urls

--- web/nl2sql_project/urls.py
+++ web/nl2sql_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import nl_query
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/query/", nl_query),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
